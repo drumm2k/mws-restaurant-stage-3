5ba8c75f4063094770a2f85d998280cd8437f844
Ruslan D <drumm2k@gmail.com>
2018-08-04 10:57:14 +0300

    chore. Removed unused module in gulp

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -8,7 +8,6 @@ const mqpacker = require('css-mqpacker'); // Pack same CSS media query rules int
 const minify = require('gulp-csso'); // Minify CSS with CSSO
 const rename = require('gulp-rename'); // Rename files easily
 const responsive = require('gulp-responsive'); // Resize and compress IMG's
-const svgmin = require('gulp-svgmin'); // Minify SVG with SVGO
 const server = require('browser-sync').create(); // Live CSS Reload & Browser Syncing
 const compression = require('compression'); // Gzip
 const uglify = require('gulp-uglify'); // Minify JS
